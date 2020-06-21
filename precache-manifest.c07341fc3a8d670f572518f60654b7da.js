6d8e8ad39a1badf7d9936ad35abfb01119446b5c
sameerasad <ssameerkhan525@gmail.com>
2020-06-21 12:48:39 +0500

    reactapp build

---
+++ precache-manifest.c07341fc3a8d670f572518f60654b7da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734e85d57a4860dcc6646597ec3b54e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e34a27b2fc7059bba93a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a92ce9b89ef60fd337bb",
+    "url": "/static/js/2.c4a99865.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4a99865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34a27b2fc7059bba93a",
+    "url": "/static/js/main.38b04d10.chunk.js"
+  },
+  {
+    "revision": "f4cf58f3503864ef774f",
+    "url": "/static/js/runtime-main.51356256.js"
+  }
+]);
